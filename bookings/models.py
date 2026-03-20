5bb245db15192c1debbb53ea0cf627287451ea68
Pablo Pérez Muñoz <pablopermun05@gmail.com>
2026-03-20 12:37:05 +0100

    feat: add Property model and user relation
    Implemented the initial Property model structure including fields for location, pricing, and capacity. Established the relationship with the custom User model using settings.AUTH_USER_MODEL.

--- bookings/models.py
+++ bookings/models.py
@@ -1,5 +1,18 @@
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Property(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    location = models.CharField(max_length=64)
+    image = models.ImageField()
+    price_per_night = models.DecimalField(max_digits=7, decimal_places=2)
+    children = models.PositiveIntegerField()
+    adults = models.PositiveIntegerField()
+    rooms = models.PositiveIntegerField()
+    allow_pets = models.BooleanField(default=False)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="properties")
