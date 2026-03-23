e8ba92198128fb498a5b1ef4e01ae528f607ce1b
Pablo Pérez Muñoz <pablopermun05@gmail.com>
2026-03-23 13:22:01 +0100

    feat: implement login and registration views and templates

--- bookings/views.py
+++ bookings/views.py
@@ -3,3 +3,53 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "bookings/index.html")
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "bookings/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "bookings/login.html")
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "bookings/register.html", {
+                "message": "Passwords must match."
+            })
+
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "bookings/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "bookings/register.html")
