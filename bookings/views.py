7376da8b80fb4807994bdaab1bb2b2d56f783c62
Pablo Pérez Muñoz <pablopermun05@gmail.com>
2026-03-24 11:16:48 +0100

    Refactor: use form.get_user() in login_view for cleaner authentication

--- bookings/views.py
+++ bookings/views.py
@@ -22,9 +22,7 @@ def login_view(request):
     if request.method == "POST":
         form = LoginForm(request, data=request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(request, username=username, password=password)
+            user = form.get_user()
             
             if user is not None:
                 login(request, user)
